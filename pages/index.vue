7f618c4754f5d75e6713efc2114c32c0092ac9dc
Mohammad Reza Hassani <reza_hassani_@outlook.com>
2022-05-01 08:21:28 +0430

    add postcss

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,18 @@
-<script lang="ts" setup>
-import { Icon } from '@iconify/vue';
-
-const { x, y } = useMouse();
-
-const count = ref(0);
-const increment = () => count.value++;
-</script>
+<script lang="ts" setup></script>
 
 <template>
-  <div class="flex flex-row">
-    <p class="font-medium text-3xl">{{ count }}</p>
-    <button
-      @click="increment"
-      class="px-5 mx-2 rounded border-solid border-2 border-gray-500"
-    >
-      <Icon icon="akar-icons:plus" />
-      <!-- <PlusIcon class="h-5 w-5" /> -->
-    </button>
-    <p>pos: {{ x }} and {{ y }}</p>
-    <FormKit type="text" />
+  <div class="flex flex-row m-6 items-center">
+    <h4 class="font-bold text-lg">pages:</h4>
+    <ul>
+      <li>
+        <nuxt-link to="/test">test</nuxt-link>
+      </li>
+    </ul>
   </div>
 </template>
+
+<style lang="postcss" scoped>
+li {
+  @apply mx-2 px-2 text-blue-700;
+}
+</style>
---
+++ pages/test.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import { Icon } from '@iconify/vue';
+
+const count = ref(0);
+
+const re = reactive({
+  count,
+  name: '',
+});
+const increment = () => count.value++;
+
+const { x, y } = useMouse();
+</script>
+
+<template>
+  <div class="flex flex-col m-6">
+    <div class="flex flex-row">
+      <p class="font-medium">{{ count }}</p>
+      <button
+        @click="increment"
+        class="px-4 mx-2 border-2 border-gray-300 rounded"
+      >
+        <Icon icon="akar-icons:plus" />
+      </button>
+    </div>
+
+    <FormKit
+      type="text"
+      placeholder="name"
+      v-model="re.name"
+      input-class="border-2 border-gray-300 rounded px-2 my-2"
+    />
+
+    {{ re }}
+    <p>mouse position: {{ x }} and {{ y }}</p>
+  </div>
+</template>
