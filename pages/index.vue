8d52e17589d1730d59d40ee919b52d753185753e
Mohammad Reza Hassani <reza_hassani_@outlook.com>
2022-05-01 06:14:16 +0430

    add pages dir

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const count = ref(0);
+const increment = () => count.value++;
+</script>
+
+<template>
+  <div class="flex flex-row">
+    <p class="font-medium text-3xl">{{ count }}</p>
+    <button
+      @click="increment"
+      class="px-5 mx-2 rounded border-solid border-2 border-gray-500"
+    >
+      +
+    </button>
+  </div>
+</template>
