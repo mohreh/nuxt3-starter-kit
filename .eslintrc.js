7eb84569639bbfb2f86028317959567611cee829
Mohammad Reza Hassani <reza_hassani_@outlook.com>
2022-05-01 06:13:53 +0430

    add tailwindcss

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,25 +5,28 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/essential",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "plugin:nuxt/recommended",
+    'plugin:vue/essential',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
   ],
   parserOptions: {
-    ecmaVersion: "latest",
-    parser: "@typescript-eslint/parser",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
   },
-  plugins: ["vue", "@typescript-eslint"],
+  plugins: ['vue', '@typescript-eslint'],
   rules: {
-    "no-unused-expressions": "off",
-    "vue/no-v-html": "off",
-    "no-console": ["error", { allow: ["warn", "error"] }],
-    "no-unused-vars": [
-      "error",
-      { vars: "all", args: "after-used", ignoreRestSiblings: false },
+    'no-unused-expressions': 'off',
+    'vue/no-v-html': 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-unused-vars': 'off',
+    'vue/no-unused-vars': 'off',
+    'vue/multi-word-component-names': [
+      'off',
+      {
+        ignores: [],
+      },
     ],
-    "vue/no-unused-vars": "off",
   },
 };
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    './app.vue',
+    './pages/**/*.vue',
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './assets/**/*.scss',
+    './assets/**/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
