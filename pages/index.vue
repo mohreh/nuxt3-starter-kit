507b8f12687c4b1fa23add75c47559d7a411f82f
Mohammad Reza Hassani <reza_hassani_@outlook.com>
2022-05-02 01:31:48 +0430

    add login

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,49 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+// const { error, data: allUsers, refresh } = await useFetch('/api/users');
+const {
+  error,
+  data: allUsers,
+  refresh,
+} = await useAsyncData('/api/users', () => $fetch('/api/users'));
+
+const loginBody = reactive({
+  email: '',
+  name: '',
+});
+const loginError = ref('');
+
+const submit = async () => {
+  const res = await useFetch('/api/users', {
+    method: 'POST',
+    body: loginBody,
+  });
+
+  if (res.error.value) {
+    loginError.value = res.error.value;
+  }
+  refresh();
+};
+</script>
 
 <template>
   <div class="flex flex-row m-6 items-center">
     <h4 class="font-bold text-lg">pages:</h4>
-    <ul>
+    <ul class="flex flex-row">
       <li>
         <nuxt-link to="/test">test</nuxt-link>
       </li>
+      <li>
+        <div class="flex flex-row">
+          <FormKit type="text" v-model="loginBody.email" placeholder="email" />
+          <FormKit type="text" v-model="loginBody.name" placeholder="name" />
+          <button @click="submit">submit</button>
+          <p v-if="error">{{ error }}</p>
+        </div>
+      </li>
     </ul>
+
+    <p v-if="error">{{ error }}</p>
+    <div v-else>{{ allUsers }}</div>
   </div>
 </template>
 
